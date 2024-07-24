fe7982c60ee4fe522ac4aab82665aad678386ea1
appyz <appyzdl5@gmail.com>
2024-07-24 09:10:07 +0530

    added Decision Tree Implementation

---
+++ DecisionTrees/train.py
@@ -0,0 +1,23 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+from DecisionTree import DecisionTree
+
+data = datasets.load_breast_cancer()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1234
+)
+
+clf = DecisionTree()
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred) / len(y_test)
+
+
+acc = accuracy(y_test, predictions)
+print("Accuracy of Decision Tree 🌲", acc)
